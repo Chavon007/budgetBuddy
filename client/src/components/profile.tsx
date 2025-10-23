d7b0dc6860eb2cabec831446737c055fce063109
azuh salvation <salvationazuh@gmail.com>
2025-10-23 21:23:49 +0100

    update

--- client/src/components/profile.tsx
+++ client/src/components/profile.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface profileData {
   firstName: string;
@@ -18,6 +18,9 @@ function Profile() {
   });
   const [success, setSuccess] = useState("");
 
+  useEffect(() => {
+    getProfile();
+  }, []);
   const getProfile = async () => {
     try {
       const res = await fetch("", {
@@ -35,7 +38,7 @@ function Profile() {
         email: data.email,
         currency: data.currency,
       });
-    } catch (err:any) {
+    } catch (err: any) {
       console.error("Can't fatch now:", err.message);
       return;
     }
@@ -69,6 +72,48 @@ function Profile() {
       console.log("Can't upadte profile now:", err.message);
     }
   };
-  return <div>h1</div>;
+  return (
+    <div className="container mx-auto">
+      <div>
+        <div>
+          <h2>
+            <span>Name</span>
+            <span>{profileData.firstName}</span>
+            <span>{profileData.lastName}</span>
+          </h2>
+          <p>
+            <span>Email:</span>
+            <span>{profileData.email}</span>
+          </p>
+          <p>
+            <span>Phone Number:</span>
+            <span>{profileData.phone}</span>
+          </p>
+          <h6>
+            <span>Currency:</span>
+            <span>{profileData.currency}</span>
+          </h6>
+        </div>
+
+        <div>
+            <form onSubmit={editProfile}>
+                {success && <p>{success}</p>}
+                <label htmlFor="">First Name</label>
+                <input type="text" value={profileData.firstName}  onChange={(e) => setProfileData({...profileData, firstName:e.target.value})} />
+
+                <label htmlFor="">Last Name</label>
+                <input type="text"  value={profileData.lastName} onChange={(e) => setProfileData({...profileData, lastName:e.target.value})}/>
+                 <label htmlFor="">Email</label>
+                <input type="text"  value={profileData.email} onChange={(e) => setProfileData({...profileData, email:e.target.value})}/>
+                 <label htmlFor="">Phone Number</label>
+                <input type="text" value={profileData.phone} onChange={(e) => setProfileData({...profileData, phone:e.target.value})}/>
+                 <label htmlFor="">Currency</label>
+                <input type="text" value={profileData.currency} onChange={(e) => setProfileData({...profileData, currency:e.target.value})}/>
+                <button type="submit">Save</button>
+            </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Profile;
